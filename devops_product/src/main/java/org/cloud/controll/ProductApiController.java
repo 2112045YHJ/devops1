a7c840166e2e7e4fb96451746b98ad9802a857b2
yhj <yhj@example.com>
2026-04-09 12:03:21 +0900

    change api

---
+++ devops_product/src/main/java/org/cloud/controll/ProductApiController.java
@@ -0,0 +1,52 @@
+package org.cloud.controll;
+
+import java.util.List;
+
+import org.cloud.dto.ProductDTO;
+import org.cloud.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/product")
+public class ProductApiController {
+
+	@Autowired	
+	private ProductService productService;
+	
+	@GetMapping("/list")
+	public List<ProductDTO> openProductList() throws Exception {
+		return productService.productList();
+	}
+	
+	@PostMapping("/insert")
+	public String insertProduct(@ModelAttribute ProductDTO product) throws Exception {
+		productService.insertProduct(product);
+		return "insert success";
+	}
+	
+	@GetMapping("/detail/{num}")
+	public ProductDTO openProductDetail(@PathVariable("num") int num) throws Exception {
+		return productService.productDetail(num);
+	}
+	
+	@PostMapping("/update")
+	public String updateProduct(@ModelAttribute ProductDTO product) throws Exception {
+		productService.updateProduct(product);
+		return "update success";
+	}
+	
+	@DeleteMapping("/delete/{num}")
+	public String deleteProduct(@PathVariable("num") int num) throws Exception {
+		productService.deleteProduct(num);
+		return "delete success";
+	}
+}
