cc3095b1fcee15e6b959a6a724c9e540359ef7ec
yhj <yhj@example.com>
2026-04-09 12:05:48 +0900

    Merge branch 'main' of https://github.com/2112045YHJ/devops1.git into main

--- devops_product/src/main/java/org/cloud/controll/ProductApiController.java
+++ devops_product/src/main/java/org/cloud/controll/ProductApiController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ModelAttribute;
 //github test pull
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/product")
 public class ProductApiController {
